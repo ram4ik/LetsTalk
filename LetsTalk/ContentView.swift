1beef5e978d72ac6930d7426666de3de913adcea
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-01 10:15:11 +0300

    init

--- LetsTalk/ContentView.swift
+++ LetsTalk/ContentView.swift
@@ -9,8 +9,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var name: String = "Tim"
     var body: some View {
-        Text("Hello World")
+        VStack {
+            TextField("Enter your name", text: $name)
+                .padding()
+            Text("Hello, \(name)")
+        }.padding()
     }
 }
 
