8c767e64bdf430ce9a7445a1328120ad237d4c7f
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-01 10:16:43 +0300

    add a border to a TextField

--- LetsTalk/ContentView.swift
+++ LetsTalk/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             TextField("Enter your name", text: $name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
             Text("Hello, \(name)")
         }.padding()
